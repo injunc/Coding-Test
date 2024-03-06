054c815e93ef1e0e1ccee48344862801ddc25613
차 인준 <injun2366@naver.com>
2024-03-06 12:12:13 +0900

    [level 0] Title: 배열 만들기 4, Time: 6.59 ms, Memory: 77 MB -BaekjoonHub

--- 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
+++ 프로그래머스/0/181918. 배열 만들기 4/배열 만들기 4.java
@@ -2,30 +2,23 @@ import java.util.*;
 
 class Solution {
     public int[] solution(int[] arr) {
-        
         // stk를 스택으로 생성
         Stack<Integer> stk = new Stack<>();
-        
         int index =0;
+        
         // arr 마지막 인덱스로 갈때까지 반복
-        while(index<arr.length){
+        while(index < arr.length){
             //stk가 비거나 작으면 arr요소 추가, 인덱스 이동
-            if(stk.isEmpty() || stk.peek() < arr[index]){
-                stk.push(arr[index++]);
-            }
+            if(stk.isEmpty() || stk.peek() < arr[index]) stk.push(arr[index++]);
             //stk가 크면 제거
-            else if(stk.peek() >= arr[index]){
-                stk.pop();
-            }           
+            else if(stk.peek() >= arr[index]) stk.pop();          
         }
         
-        // 배열 변환
+        // 배열로 변환
         int[]answer = new int[stk.size()];
         int ai = 0;
         
-        for(int n : stk){
-            answer[ai++] = n;
-        }
-        return answer;
+        for(int n : stk)  answer[ai++] = n;
+        return stk.stream().mapToInt(Integer::intValue).toArray();
     }
 }
