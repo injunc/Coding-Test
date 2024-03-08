92d70f84470ea0d10b6a784d7313af04de71be49
차 인준 <injun2366@naver.com>
2024-03-08 15:19:09 +0900

    [level 0] Title: 문자 개수 세기, Time: 0.02 ms, Memory: 78.7 MB -BaekjoonHub

--- 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
+++ 프로그래머스/0/181902. 문자 개수 세기/문자 개수 세기.java
@@ -5,10 +5,10 @@ class Solution {
         // 문자열 순회  
         for(char c : my_string.toCharArray()){   
             // 대문자 -65 소문자 -97+26  인덱스  위치에 값 카운트
-            if(c > 96){
-                answer[c-97+26]++;
+            if(c > 'a'-1){
+                answer[c-'a'+26]++;
             }else{
-                answer[c-65]++;
+                answer[c-'A']++;
             }
         }
         return answer;
