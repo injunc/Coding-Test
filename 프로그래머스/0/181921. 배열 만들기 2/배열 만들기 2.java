f9e748f4e5c1ea82c1edbd574b78446500d7eff2
차 인준 <injun2366@naver.com>
2024-03-08 22:42:05 +0900

    [level 0] Title: 배열 만들기 2, Time: 41.75 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
+++ 프로그래머스/0/181921. 배열 만들기 2/배열 만들기 2.java
@@ -3,22 +3,20 @@ import java.util.*;
 class Solution {
     public int[] solution(int l, int r) {
         List<Integer> answer = new ArrayList<>();
-
+        
         // (l ~ r) 순회
         for(int i =l; i<=r; i++){
             // 문자열로 변환
             String s = i+"";
+            if(s.charAt(s.length()-1) != '0' && s.charAt(s.length()-1) != '5') continue;
+            
             // 0 5 를 ""으로 교체
             s = s.replaceAll("5", "");
             s = s.replaceAll("0", "");
             // "" 값만 남았다면 저장
             if(s.equals("")) answer.add(i);
         }
-        if(answer.size()<1){
-            int[] re = {-1};
-            return re;
-        }
-
-        return answer.stream().mapToInt(a->a).toArray();
+        
+        return answer.size()>0?answer.stream().mapToInt(a->a).toArray():new int[]{-1};
     }
 }
