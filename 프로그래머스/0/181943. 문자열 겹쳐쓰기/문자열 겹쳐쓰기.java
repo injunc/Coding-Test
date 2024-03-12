40e464b122a10164d7715e0f3efa7fdb67db5c6e
차 인준 <injun2366@naver.com>
2024-03-12 22:29:04 +0900

    [level 0] Title: 문자열 겹쳐쓰기, Time: 0.05 ms, Memory: 78.1 MB -BaekjoonHub

--- 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
+++ 프로그래머스/0/181943. 문자열 겹쳐쓰기/문자열 겹쳐쓰기.java
@@ -1,14 +1,11 @@
 class Solution {
     public String solution(String my_string, String overwrite_string, int s) {
         
-        // my_string s-1까지 자르기
-        String s1 = my_string.substring(0 , s);
+        StringBuilder sb = new StringBuilder(my_string);
         
-        // my_string s+overwirte 길이 까지 자르기
-        String s2 = my_string.substring(s+overwrite_string.length() , my_string.length());
-        // 합치기
-        
-       
-        return s1 + overwrite_string + s2;
+        sb = sb.delete(s, s+overwrite_string.length());
+        sb = sb.insert(s, overwrite_string);
+
+        return sb.toString();
     }
 }
