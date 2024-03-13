0cd26c1f7eb4fe19f1bbc23f6bf29fbddf157d8f
차 인준 <injun2366@naver.com>
2024-03-13 14:37:38 +0900

    [level 0] Title: 주사위 게임 3, Time: 3.36 ms, Memory: 77.6 MB -BaekjoonHub

--- 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
+++ 프로그래머스/0/181916. 주사위 게임 3/주사위 게임 3.java
@@ -2,12 +2,13 @@ import java.util.*;
 
 class Solution {
     public int solution(int a, int b, int c, int d) {
+        List<Integer> list = new ArrayList<>(Arrays.asList(a,b,c,d));
         int answer = 1;
         
         //map 사용해서 갯수 저장
         Map<Integer,Integer>map = new HashMap<>();
         
-        for(int q : Arrays.asList(a,b,c,d)){
+        for(int q : list){
             map.put(q, map.getOrDefault(q,0)+1);
         }
         
@@ -18,7 +19,7 @@ class Solution {
         if(size == 1) return a*1111;    
         
         // 1:1:1:1 이면
-        if(size == 4) return Arrays.asList(a,b,c,d).stream().mapToInt(i->i).min().getAsInt();
+        if(size == 4) return list.stream().mapToInt(i->i).min().getAsInt();
         
         // 2:1:1 이면
         if(size == 3){
@@ -36,12 +37,12 @@ class Solution {
         
         // 2:2 이면
         if(map.get(a) == 2){
-            int[] sum = Arrays.asList(a,b,c,d).stream().distinct().mapToInt(i->i).toArray();
+            int[] sum = list.stream().distinct().mapToInt(i->i).toArray();
             return (sum[0]+sum[1])*Math.abs(sum[0]-sum[1]);
             
         }else{
             //3:1 이면
-            int[] sum = Arrays.asList(a,b,c,d).stream().distinct().mapToInt(i->i).toArray();
+            int[] sum = list.stream().distinct().mapToInt(i->i).toArray();
             int mul = map.get(sum[0])==3?sum[0]:sum[1];
             int sol = mul==sum[0]?sum[1]:sum[0];
             
