06a08fa066e085d1a95d18a90c2d3ee0bd82ae78
차 인준 <injun2366@naver.com>
2024-03-13 10:21:08 +0900

    [level 0] Title: 전국 대회 선발 고사, Time: 0.57 ms, Memory: 77.7 MB -BaekjoonHub

--- 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
+++ 프로그래머스/0/181851. 전국 대회 선발 고사/전국 대회 선발 고사.java
@@ -9,23 +9,12 @@ class Solution {
             player[i][1] = attendance[i]?1:0;
             player[i][2] = i;
         }
-        
-        Comparator<int[]> cp  = new Comparator<int[]>(){
-            
-            @Override
-            public int compare(int[] a1, int[] a2){
-                if(a2[1] == 0) return -1;
-                if(a1[1] == 0) return 1;
-                
-                return a1[0] - a2[0];
-            }
-        };
-        
-        Arrays.sort(player, cp);
-        
-        // for(int[] ap : player){
-        //     System.out.println(ap[0]+":"+ap[1]+":"+ap[2]);
-        // }
+
+        Arrays.sort(player, (a1, a2) -> {
+            if(a2[1] == 0) return -1;
+            if(a1[1] == 0) return 1;
+            return a1[0] - a2[0];
+        });
         
         return player[0][2]*10000 + player[1][2]*100 +  player[2][2];
     }
