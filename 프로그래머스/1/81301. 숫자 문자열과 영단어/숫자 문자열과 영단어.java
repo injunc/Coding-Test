ecd4725aeeb24958553b6a9dcda85a30cd6b62b2
차 인준 <injun2366@naver.com>
2024-03-14 15:51:36 +0900

    [level 1] Title: 숫자 문자열과 영단어, Time: 0.13 ms, Memory: 73.6 MB -BaekjoonHub

--- 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
+++ 프로그래머스/1/81301. 숫자 문자열과 영단어/숫자 문자열과 영단어.java
@@ -2,13 +2,31 @@ import java.util.*;
 
 class Solution {
     public int solution(String s) {
-         String[] strArr = {"zero", "one", "two", "three", "four", "five", "six", "seven", "eight", "nine"};
-        for(int i = 0; i < strArr.length; i++) {
-            s = s.replaceAll(strArr[i], Integer.toString(i));
+        StringBuilder answer = new StringBuilder();
+        StringBuilder str = new StringBuilder();
+        
+        // 매핑 배열 생성
+        List<String> nums = new ArrayList<>(Arrays.asList( 
+            "zero", "one", "two", "three",
+            "four", "five", "six", "seven",
+            "eight", "nine"));
+        
+        // 문자열 순회
+        for(char c : s.toCharArray()){  
+            // 숫자는 그냥 추가
+            if(c <= '9'){
+                answer.append(c);
+            }else{
+                // 문자 하나씩 연결
+                str.append(c);
+                
+                // 문자열이 숫자랑 매핑이 되는지 확인
+                int n = nums.indexOf(str.toString());
+                if(n == -1) continue;
+                answer.append(n+"");
+                str.setLength(0);
+            }
         }
-        return Integer.parseInt(s);
-
-
-       
+        return Integer.parseInt(answer.toString());
         }
     }
