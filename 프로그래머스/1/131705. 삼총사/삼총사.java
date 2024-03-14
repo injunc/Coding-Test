9716fe03189745e36a89448d3deaefe8138bd08a
차 인준 <injun2366@naver.com>
2024-03-14 13:39:58 +0900

    [level 1] Title: 삼총사, Time: 0.03 ms, Memory: 74.9 MB -BaekjoonHub

--- 프로그래머스/1/131705. 삼총사/삼총사.java
+++ 프로그래머스/1/131705. 삼총사/삼총사.java
@@ -1,10 +1,10 @@
 class Solution {
-    public int solution(int[] number) {
+    public int solution(int[] n) {
         int count =0;
-        for(int i=0; i<number.length-2; i++){
-            for(int j=i+1; j<number.length-1; j++){
-                for(int k=j+1; k<number.length; k++){
-                    if(number[i]+number[j]+number[k] == 0) count++;
+        for(int i=0; i<n.length-2; i++){
+            for(int j=i+1; j<n.length-1; j++){
+                for(int k=j+1; k<n.length; k++){
+                    if(n[i]+n[j]+n[k] == 0) count++;
                 }
             }
         }
