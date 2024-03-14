e6a2e2b169fc91ea5962a270cdce8fedfc1ffbc4
차 인준 <injun2366@naver.com>
2024-03-14 14:06:17 +0900

    [level 1] Title: 시저 암호, Time: 1.25 ms, Memory: 79.4 MB -BaekjoonHub

--- 프로그래머스/1/12926. 시저 암호/시저 암호.java
+++ 프로그래머스/1/12926. 시저 암호/시저 암호.java
@@ -1,27 +1,19 @@
 class Solution {
     public String solution(String s, int n) {
-        // 48
-        // 65
-        // 97
-        String answer = "";
-        
-        // System.out.println((int)'z'); // 122
-       // System.out.println((int)'Z'); // 90
+        StringBuilder answer = new StringBuilder();
+
         // 문자 순회
         for(char c : s.toCharArray()){
             // 공백 무시
-            if(c == ' ') answer+=c;
+            if(c == ' ') answer.append(c);
             else{
                 // 문자 + n / 26
                 int ch = (c+n);
-                if(c >= 97){
-                    ch = ch>'z'?(ch+'a'-'z'-1):ch;
-                }else{
-                    ch = ch>'Z'?(ch+'A'-'Z'-1):ch;
-                }
-                answer += (char)ch;
+                if(c >= 97) ch = ch>'z'?(ch+'a'-'z'-1):ch;
+                else ch = ch>'Z'?(ch+'A'-'Z'-1):ch;
+                answer.append((char)ch);
             }
 		}
-        return answer;
+        return answer.toString();
     }
 }
