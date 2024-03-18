e42926c6b565d0c7b7381f37a29b44fb739e3d61
차 인준 <injun2366@naver.com>
2024-03-18 21:11:07 +0900

    [Bronze I] Title: 약수, Time: 124 ms, Memory: 14336 KB -BaekjoonHub

---
+++ 백준/Bronze/1037. 약수/약수.java
@@ -0,0 +1,27 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.Arrays;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int n = Integer.parseInt(br.readLine()); 
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		int[] arr = new int[n];
+		
+		for(int i=0; i<n;i++) {
+			arr[i] = Integer.parseInt(st.nextToken());
+		}
+		
+		Arrays.sort(arr);
+		
+		if(n == 1) {
+			System.out.println(arr[0]*arr[0]);
+		}else {
+			System.out.println(arr[0]*arr[n-1]);
+		}
+		
+	}
+}
