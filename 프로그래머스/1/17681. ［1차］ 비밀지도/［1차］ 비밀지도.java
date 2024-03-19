2c68746379fb455269f183cb1ca21bcdd1cf2466
차 인준 <injun2366@naver.com>
2024-03-19 21:29:08 +0900

    [level 1] Title: [1차] 비밀지도, Time: 7.51 ms, Memory: 74.6 MB -BaekjoonHub

--- 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
+++ 프로그래머스/1/17681. ［1차］ 비밀지도/［1차］ 비밀지도.java
@@ -2,15 +2,25 @@ class Solution {
     public String[] solution(int n, int[] arr1, int[] arr2) {
         String[] answer = new String[n];
         
-        for(int i=0; i<n; i++){
-            int l = arr1[i] | arr2[i];
-            long sum = (long)l;
-            String s = String.format("%0" + n + "d", Long.parseLong(Long.toBinaryString(sum)));            
+        System.out.println(Integer.MAX_VALUE);
+
+        // 배열 순회
+        for(int i =0; i< n; i++){    
+            // arr1 arr2 | 연산
+            int code = arr1[i] | arr2[i];
             
-            s =s.replaceAll("1", "#");
-            s =s.replaceAll("0", " ");
+            // n길이 만큼 비트 문자열로 변환
+            long bs = Long.parseLong(Integer.toBinaryString(code));
+            String reg = "%0"+n+"d";
+            String map = String.format(reg, bs);
             
-            answer[i] = s;
+            // 1은 # 변환
+            map = map.replaceAll("1", "#");
+            // 0은 공백 변환
+            map = map.replaceAll("0", " ");
+            
+            // answer에 추가
+            answer[i] = map;
         }
         
         return answer;
