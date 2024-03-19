36b1c77ee62a43b85b93dc4546e2113c6ab2c1ce
차 인준 <injun2366@naver.com>
2024-03-19 13:50:09 +0900

    [level 1] Title: 콜라 문제, Time: 0.02 ms, Memory: 75.7 MB -BaekjoonHub

--- 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
+++ 프로그래머스/1/132267. 콜라 문제/콜라 문제.java
@@ -2,12 +2,10 @@ class Solution {
     public int solution(int a, int b, int n) {
         int num =0;
         
-        while(n >= a){
-            int give = n/ a;    // 반환할거
-            int less = n% a;    // 남은거
+        while(n >= a){ 
             int get =  n/a *b;  // 받은거
             num += get;
-            n = n- (n-less) +get;
+            n = n- (n-(n%a)) +get;
         }
         return num;
     }
