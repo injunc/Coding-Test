82354e78ede22836fc4ce31d327de9d29d864db7
차 인준 <injun2366@naver.com>
2024-03-21 15:19:15 +0900

    [level 1] Title: 기사단원의 무기, Time: 1.74 ms, Memory: 77.6 MB -BaekjoonHub

---
+++ 프로그래머스/1/136798. 기사단원의 무기/기사단원의 무기.java
@@ -0,0 +1,27 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int number, int limit, int power) {      
+        int answer =0;
+        
+        // number 순회
+        for(int i=1; i<= number; i++){
+            // 약수 배열 반환
+            answer += lit(i, limit, power);
+        }
+        return answer;
+}
+    
+    public int lit(int n,int limit, int power){
+        if(n== 1) return 1;
+        
+        Set<Integer> set = new HashSet<>();
+        for(int i =1; i<= Math.sqrt(n); i++){
+            if(n%i ==0 ){
+                set.add(i);
+                set.add(n/i);
+            }
+        }
+        return set.size()>limit?power:set.size();
+    }
+}
