e1d8f7857720b40f6f474095286a5ee3c61917a6
차 인준 <injun2366@naver.com>
2024-03-21 14:49:49 +0900

    [level 1] Title: 과일 장수, Time: 3.74 ms, Memory: 73.5 MB -BaekjoonHub

--- 프로그래머스/1/135808. 과일 장수/과일 장수.java
+++ 프로그래머스/1/135808. 과일 장수/과일 장수.java
@@ -2,20 +2,16 @@ import java.util.*;
 
 class Solution {
     public int solution(int k, int m, int[] score) {
-        int answer = 0;
-        int score1[] = new int[score.length];
-		int box = score.length / m;
-		
-		Arrays.sort(score);
-		for(int i=score.length-1; i>=0; i--) {
-			score1[score.length-i-1] = score[i];
-		}
-
-	    for(int i=0; i< box; i++) {
-	    	int[] a = Arrays.copyOfRange(score1, i*m, i*m+m);
-	    	int value = Arrays.stream(a).min().getAsInt();
-	    	answer += value*m;
-	    }
+        int answer =0;
+        
+        // 내림차순 정렬
+        score = Arrays.stream(score).boxed().sorted(Comparator.reverseOrder()).mapToInt(i->i).toArray();
+        
+        // 배열 순회
+        int limit = score.length-score.length%m;
+        for(int i =0; i<limit; i+=m){
+            answer += m*score[i+m-1];
+        }
         
         return answer;
     }
