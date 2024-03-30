593c08aad1ff40fc75ee1ebc7ce00099768a7d57
차 인준 <injun2366@naver.com>
2024-03-31 00:41:52 +0900

    [Bronze II] Title: Hashing, Time: 128 ms, Memory: 14216 KB, Score: 100 point -BaekjoonHub

--- 백준/Bronze/15829. Hashing/Hashing.java
+++ 백준/Bronze/15829. Hashing/Hashing.java
@@ -8,19 +8,19 @@ public class Main {
     	BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
     	int n = Integer.parseInt(br.readLine());
     	String input = br.readLine();
-    	
     	char[] crr = input.toCharArray();
     	
     	long answer = 0;
-    	
+        long pow =1;
+        int r = 31;
+        int m = 1234567891;
+        
     	for(int i =0; i<n; i++) {
     		long num = (long)(crr[i]-96);
-    		long d = (long)Math.pow(31, i);
-    		num *= (long)d;
-    		answer += num;
+    		answer += num*pow % m;
+            pow = pow*r % m;
     	}
-    	
-    	System.out.println(answer);
+    	System.out.println(answer % m);
     }
 
 }
