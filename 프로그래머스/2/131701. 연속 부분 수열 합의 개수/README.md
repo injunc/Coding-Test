c0b78845c428c53970654cc8c9c6e154de6d4c13
차 인준 <injun2366@naver.com>
2024-05-06 23:25:04 +0900

    [level 2] Title: 연속 부분 수열 합의 개수, Time: 1514.33 ms, Memory: 118 MB -BaekjoonHub

--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/README.md
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/README.md
@@ -1,10 +1,10 @@
 # [level 2] 연속 부분 수열 합의 개수 - 131701 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131701?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/131701) 
 
 ### 성능 요약
 
-메모리: 119 MB, 시간: 81.61 ms
+메모리: 118 MB, 시간: 1514.33 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2024년 05월 06일 23:10:21
+2024년 05월 06일 23:25:02
 
 ### 문제 설명
 
--- 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
+++ 프로그래머스/2/131701. 연속 부분 수열 합의 개수/연속 부분 수열 합의 개수.java
@@ -1,36 +1,20 @@
+
 import java.util.*;
 
 class Solution {
     public int solution(int[] elements) {
         Set<Integer> set = new HashSet<>();
-        int len = elements.length;
-         
-        for(int e : elements) set.add(e);
-        
-        // elements 길이만큼 반복
-        for(int i =1; i<len; i++){
-            // 슬라이딩 윈도우 시작 0 ~ 마지막
-            int head = 0;
-            int tail = head+i;
-            
-            int sum =0;
-            for(int s=0; s<=tail; s++){
-                sum+= elements[s];
-            }
-            
-            // head 값  끝까지 이동
-            while(head < len){   
-                // 앞에 값 제거, 뒤에 값 추가
-                sum -= elements[head++];
-                tail = (++tail)%len;
-                sum += elements[tail];
-                
-                // set에 저장
+
+        for (int i=1; i<=elements.length; i++) {
+            for (int j=0; j<elements.length; j++) {
+                int sum = 0;
+                for (int k=j; k<j+i; k++) {
+                    sum += elements[k%elements.length];
+                }
                 set.add(sum);
             }
         }
+
         return set.size();
     }
 }
-
-    
